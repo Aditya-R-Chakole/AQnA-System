73734a6a761ae0b880ae2c22c3b02971badf4b55
Aditya-R-Chakole <57232967+Aditya-R-Chakole@users.noreply.github.com>
2022-01-22 00:23:41 +0530

    Update distillbert.py

--- distillbert.py
+++ distillbert.py
@@ -437,7 +437,12 @@ def getList(dict):
     return list
 
 ### Code
-st.set_page_config(layout='wide')
+st.set_page_config(
+    page_title="eSeller",
+    page_icon="https://github.com/Aditya-R-Chakole/AQnA-System/blob/main/seller-png.png?raw=true",
+    layout="wide",
+)
+
 st.markdown("""
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
